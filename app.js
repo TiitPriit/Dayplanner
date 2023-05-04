0faa5af6382b85c853a49fc05fe598368b2c5393
Oskar.Kallas <oskar.kallas@ametikool.ee>
2023-05-04 09:29:25 +0300

    final

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
-// Get the daily schedule table and time inputs
-var dailyScheduleTable = document.getElementById("daily-schedule");
-var timeInputs = document.getElementsByClassName("time-input");
-var eventInputs = document.getElementsByClassName("event-input");
 
-// Load the daily schedule from local storage, if available
-var dailySchedule = JSON.parse(localStorage.getItem("dailySchedule"));
-if (dailySchedule) {
-	for (var i = 0; i < dailySchedule.length; i++) {
-		timeInputs[i].value = dailySchedule[i].time;
-		eventInputs[i].value = dailySchedule[i].event;
-	}
+	function calculateTime() {
+	let task1Name = document.getElementById("task1Name").value;
+	let task2Name = document.getElementById("task2Name").value;
+	let task3Name = document.getElementById("task3Name").value;
+	let task4Name = document.getElementById("task4Name").value;
+	let task5Name = document.getElementById("task5Name").value;
+	let task1 = Number(document.getElementById("task1").value);
+	let task2 = Number(document.getElementById("task2").value);
+	let task3 = Number(document.getElementById("task3").value);
+	let task4 = Number(document.getElementById("task4").value);
+	let task5 = Number(document.getElementById("task5").value);
+	let totalSpentTime = task1 + task2 + task3 + task4 + task5;
+	let timeLeft = 24 - totalSpentTime;
+	document.getElementById("timeLeft").value = timeLeft + " hours";
+
+	let data = {
+		labels: [task1Name, task2Name, task3Name, task4Name, task5Name],	
+		datasets: [
+			{
+				label: "Time spent",
+				data: [task1, task2, task3, task4, task5],
+				backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#FF5733", "#9FFF33"]
+			}
+		]
+	};
+
+	let options = {
+		title: {
+			display: true,
+			text: "Time spent on each task"
+		}
+	};
+
+	let ctx = document.getElementById("pieChart").getContext("2d");
+	let pieChart = new Chart(ctx, {
+		type: "pie",
+		data: data,
+		options: options
+	});
 }
-
-// Save the daily schedule to local storage when the Save button is clicked
-document.getElementById("save-button").addEventListener("click", function() {
-	var dailySchedule = [];
-	for (var i = 0; i < timeInputs.length; i++) {
-		dailySchedule.push({
-			time: timeInputs[i].value,
-			event: eventInputs[i].value
-		});
-	}
-	localStorage.setItem("dailySchedule", JSON.stringify(dailySchedule));
-	alert("Daily schedule saved.");
-});
-
-// Clear the daily schedule when the Clear button is clicked
-document.getElementById("clear-button").addEventListener("click", function() {
-	localStorage.removeItem("dailySchedule");
-	alert("Daily schedule cleared.");
-});
